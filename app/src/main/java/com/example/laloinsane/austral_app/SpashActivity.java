b7522d4782f10441e2f4b7534511dedec3eaed38
laloinsane <calisto.michel@gmail.com>
2018-11-04 12:48:35 -0300

    integracion de splash screen

---
+++ app/src/main/java/com/example/laloinsane/austral_app/SpashActivity.java
@@ -0,0 +1,23 @@
+package com.example.laloinsane.austral_app;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpashActivity extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SpashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
